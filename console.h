dbfbcd7bbcbb3103226f4bdca7b60cc1f0a852c4
7amok4a <123641786+7amok4a@users.noreply.github.com>
2023-12-08 17:13:27 +0200

    Add files via upload

---
+++ console.h
@@ -0,0 +1,15 @@
+//
+// Created by matter on 05/12/23.
+//
+
+#ifndef HOSPITAL_CONSOLE_H
+#define HOSPITAL_CONSOLE_H
+void console () {
+    std :: cout << "Enter your choice:\n" ;
+    std :: cout << "1) Add new patinet\n" ;
+    std :: cout << "2) Print all patinet\n" ;
+    std :: cout << "3) Get next patinet\n" ;
+    std :: cout << "4) Exit\n" ;
+}
+
+#endif //HOSPITAL_CONSOLE_H
